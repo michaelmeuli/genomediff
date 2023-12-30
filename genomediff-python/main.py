b446544f9612e00089bb7b83609d03c50883b0fd
Michael Meuli <michael.meuli@gmail.com>
2023-12-30 09:08:21 +0100

    update

--- genomediff-python/main.py
+++ genomediff-python/main.py
@@ -1,11 +1,22 @@
 #! /usr/bin/env python3
 
+import os
 from genomediff import *
 
 document = GenomeDiff.read(open('annotated.gd', 'r', encoding='utf-8'))
-#print(document.metadata)
+print(document.metadata)
 
 print(document.mutations[0].type)
 
 
 print(repr(document.mutations[0]))
+
+document = GenomeDiff.read(open('annotated.gd', 'r', encoding='utf-8'))
+print(document.metadata['READSEQ'])
+
+
+
+file_path = document.metadata['READSEQ']
+filename = os.path.basename(file_path)
+sample_id = filename.split('_')[0]
+print(sample_id)
